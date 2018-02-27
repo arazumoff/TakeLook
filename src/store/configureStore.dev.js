52561084a997316a982ed063ec906af2648d8346
Aleksander Razumov <a.razumoff@gmail.com>
2018-02-27 16:57:53 +0300

    fix store

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,29 +1,25 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import createSagaMiddleware, { END } from 'redux-saga'
-import rootReducer from '../reducers'
+import { createStore, applyMiddleware} from "redux"
+import createSagaMiddleware, { END } from "redux-saga"
+import rootReducer from "../reducers"
 
 
 export default function configureStore(initialState) {
-    const sagaMiddleware = createSagaMiddleware()
+    const sagaMiddleware = createSagaMiddleware();
 
     const store = createStore(
         rootReducer,
         initialState,
-        compose(
-            applyMiddleware(
-                sagaMiddleware,
-            ),
-        )
-    )
+        applyMiddleware(sagaMiddleware)
+    );
 
     if (module.hot) {
         // Enable Webpack hot module replacement for reducers
-        module.hot.accept('../reducers', () => {
-            const nextRootReducer = require('../reducers').default
-            store.replaceReducer(nextRootReducer)
+        module.hot.accept("../reducers", () => {
+            const nextRootReducer = require("../reducers").default;
+            store.replaceReducer(nextRootReducer);
         });
     }
-    store.runSaga = sagaMiddleware.run
-    store.close = () => store.dispatch(END)
+    store.runSaga = sagaMiddleware.run;
+    store.close = () => store.dispatch(END);
     return store;
 }
