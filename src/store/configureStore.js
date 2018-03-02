c715438ad19775f7c64f97ba59736d1e9dccd103
Aleksander Razumov <a.razumoff@gmail.com>
2018-03-02 10:50:28 +0300

    join to single store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,16 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./configureStore.prod')
-} else {
-    module.exports = require('./configureStore.dev')
+import { createStore, applyMiddleware } from "redux"
+import createSagaMiddleware, { END } from "redux-saga"
+import rootReducer from "../reducers"
+
+export default function configureStore(initialState) {
+    const sagaMiddleware = createSagaMiddleware();
+    const store = createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(sagaMiddleware)
+    );
+
+    store.runSaga = sagaMiddleware.run;
+    store.close = () => store.dispatch(END);
+    return store;
 }
